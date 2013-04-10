c67f1b2ed5d2964085ab7180a7ac219b70eb99a1
Guille Paz <guille87paz@gmail.com>
2013-04-10 09:11:29 -0300

    #5 Update standalone version.

--- standalone/camera-action.js
+++ standalone/camera-action.js
@@ -1,3 +1,4 @@
+(function(e){'use strict';function t(){this.collection={};this.maxListeners=10}t.prototype.addListener=function(e,t,n){if(e===undefined){throw new Error('jvent - "addListener(event, listener)": It should receive an event.')}if(t===undefined){throw new Error('jvent - "addListener(event, listener)": It should receive a listener function.')}var r=this.collection;t.once=n||false;if(r[e]===undefined){r[e]=[]}if(r[e].length+1>this.maxListeners&&this.maxListeners!==0){throw new Error("Warning: So many listeners for an event.")}r[e].push(t);this.emit("newListener");return this};t.prototype.on=t.prototype.addListener;t.prototype.once=function(e,t){this.on(e,t,true);return this};t.prototype.removeListener=function(e,t){if(e===undefined){throw new Error('jvent - "removeListener(event, listener)": It should receive an event.')}if(t===undefined){throw new Error('jvent - "removeListener(event, listener)": It should receive a listener function.')}var n=this.collection[e],r=0,i;if(n!==undefined){i=n.length;for(r;r<i;r+=1){if(n[r]===t){n.splice(r,1);break}}}return this};t.prototype.off=t.prototype.removeListener;t.prototype.removeAllListeners=function(e){if(e===undefined){throw new Error('jvent - "removeAllListeners(event)": It should receive an event.')}delete this.collection[e];return this};t.prototype.setMaxListeners=function(e){if(isNaN(e)){throw new Error('jvent - "setMaxListeners(n)": It should receive a number.')}this.maxListeners=e;return this};t.prototype.listeners=function(e){if(e===undefined){throw new Error('jvent - "listeners(event)": It should receive an event.')}return this.collection[e]};t.prototype.emit=function(){var e=Array.prototype.slice.call(arguments,0),t=e.shift(),n,r=0,i;if(t===undefined){throw new Error('jvent - "emit(event)": It should receive an event.')}if(typeof t==="string"){t={type:t}}if(!t.target){t.target=this}if(this.collection[t.type]!==undefined){n=this.collection[t.type];i=n.length;for(r;r<i;r+=1){n[r].apply(this,e);if(n[r].once){this.off(t.type,n[r]);i-=1;r-=1}}}return this};if(typeof e.define==="function"&&e.define.amd!==undefined){e.define("Jvent",[],function(){return t})}else if(typeof module!=="undefined"&&module.exports!==undefined){module.exports=t}else{e.Jvent=e.EventEmitter=t}})(this);
 (function (window) {
     'use strict';
 
@@ -22,6 +23,8 @@
 
         return this;
     }
+
+    Camera.prototype = new Jvent();
 
     Camera.prototype.init = function (el, options) {
         // Normalize options
@@ -69,9 +72,7 @@
 
             navigator.getUserMedia({'video': this._constraints, 'audio': false}, function (LocalMediaStream) {
                 that._success(LocalMediaStream);
-            }, function () {
-                that._fail();
-            });
+            }, that._fail);
         }
 
         return this;
@@ -83,18 +84,22 @@
         this._video.play();
         this._stream = LocalMediaStream;
 
+        this.emit('action');
+
         return this;
 
     };
 
     Camera.prototype._fail = function () {
-        window.console.log('Your browser doesn\'t support this feature.');
+        this.emit('fail');
 
         return this;
     };
 
     Camera.prototype.pause = function () {
         this._video.pause();
+
+        this.emit('pause');
 
         return this;
     };
@@ -104,6 +109,8 @@
         this._stream.stop();
         this._stream = undefined;
         this.el.removeChild(this._video);
+
+        this.emit('cut');
 
         return this;
     };
@@ -126,12 +133,26 @@
             document.location.href = strImage.replace(options.type, 'image/octet-stream');
         }
 
+        this.emit('photo', strImage);
+
         return strImage;
     };
 
     /**
      * Expose Camera
      */
-    exports = module.exports = Camera;
+    // AMD suppport
+    if (typeof window.define === 'function' && window.define.amd !== undefined) {
+        window.define('camera', [], function () {
+            return Camera;
+        });
 
+    // CommonJS suppport
+    } else if (typeof module !== 'undefined' && module.exports !== undefined) {
+        module.exports = Camera;
+
+    // Default
+    } else {
+        window.Camera = Camera;
+    }
 }(this));
